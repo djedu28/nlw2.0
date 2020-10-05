5ca429ef8cf98eb75e309cc2d500ba7cb3b554a4
djedu28 <43933482+djedu28@users.noreply.github.com>
2020-10-05 10:42:39 -0300

    Add files via upload

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,15 @@
+import React, {ReactElement} from 'react';
+
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+
+function TeacherForm(): ReactElement {
+	
+	return (
+	<h1>aa</h1>
+	
+	);
+}
+
+export default TeacherForm;
